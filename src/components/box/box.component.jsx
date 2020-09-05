e7e4da743c47cc4b176b8471b13feaa9cbabbab6
jerinnishokc <jerinnishok@outlook.com>
2020-09-05 18:32:26 +0530

    Made major changes to the home screen and added many animations

--- src/components/box/box.component.jsx
+++ src/components/box/box.component.jsx
@@ -3,12 +3,16 @@ import './box.styles.css';
 
 const Box = (props) => {
     return (
-        <div className="box-container" style={{"background":props.service.color}}>            
+        <div className="box-container">
+            <div className={`box-text-container-${props.service.id % 2 === 0 ? 'even': 'odd'}`} style={{"background":props.service.color}}>
             <h1>{props.service.name}</h1>            
+            {props.service.id % 2 === 0 ? <hr />: null} 
+            <p>{props.service.text}</p>            
+            </div>                        
             {
                 !!props.service.imgUrl ? 
-                <div className="box-image-container" style={{"background":`url(${props.service.imgUrl})`,"background-position": "top left",
-                "background-size": "cover","background-repeat":"no-repeat"}}>                    
+                <div className={`box-image-container-${props.service.id % 2 === 0 ? 'even': 'odd'}`} style={{"background":`url(${props.service.imgUrl})`,"backgroundPosition": "top left",
+                "backgroundSize": "cover","backgroundRepeat":"no-repeat"}}>                    
                 </div>
                 : null
             }
--- src/pages/home/homepage.component.jsx
+++ src/pages/home/homepage.component.jsx
@@ -4,21 +4,23 @@ import Navbar from '../../components/main-nav/mainnav.component';
 import './homepage.styles.css';
 import Footer from '../../components/footer/footer.component';
 import { ReactComponent as LandingSVG } from '../../assets/undraw_order_a_car_3tww.svg';
+import { ReactComponent as UserServiceSVG } from '../../assets/undraw_order_ride_xjs4.svg';
+import { ReactComponent as VendorServiceSVG } from '../../assets/undraw_Vehicle_sale_a645.svg';
 import Box from '../../components/box/box.component';
 
 class HomePage extends React.Component {
     static defaultProps = {
       services: [
-        {id:1, name: "A", color: "red", imgUrl: "https://images.unsplash.com/photo-1482029255085-35a4a48b7084?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"},
-        {id:2, name: "B", color: "green", imgUrl: "https://images.unsplash.com/photo-1590253230376-315df106ebeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"},
-        {id:3, name: "C", color: null, imgUrl: "https://images.unsplash.com/photo-1563720359838-8d9cdd36ceab?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"},
-        {id:4, name: "D", color: "pink", imgUrl: "https://images.unsplash.com/photo-1554672408-730436b60dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"},
-        {id:5, name: "E", color: "blue"},
-        {id:6, name: "F", color: "indigo"},
-        {id:7, name: "G", color: "purple", imgUrl: "https://images.unsplash.com/photo-1541376936666-59ec998f1afd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=687&q=80"},
-        {id:8, name: "H", color: "lightgreen"},
-        {id:9, name: "H", color: "gray"},
-        {id:10, name: "H", color: "aquamarine", imgUrl: "https://images.unsplash.com/photo-1518430272387-254558840136?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=699&q=80"}
+        {id:1, name: "Automobile", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", color: "red", imgUrl: "https://images.unsplash.com/photo-1482029255085-35a4a48b7084?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"},
+        {id:2, name: "Rent", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "green", imgUrl: "https://images.unsplash.com/photo-1590253230376-315df106ebeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"},
+        {id:3, name: "Premium service", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: 'yellow', imgUrl: "https://images.unsplash.com/photo-1563720359838-8d9cdd36ceab?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"},
+        {id:4, name: "Ease of use", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", color: "pink", imgUrl: "https://images.unsplash.com/photo-1554672408-730436b60dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"},
+        {id:5, name: "Security", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "blue", imgUrl: "https://images.unsplash.com/photo-1554672408-730436b60dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"},
+        {id:6, name: "Track your vehicle", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "indigo", imgUrl: "https://images.unsplash.com/photo-1554672408-730436b60dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"},
+        {id:7, name: "GPS", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "purple", imgUrl: "https://images.unsplash.com/photo-1541376936666-59ec998f1afd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=687&q=80"},
+        {id:8, name: "H", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "lightgreen", imgUrl: "https://images.unsplash.com/photo-1554672408-730436b60dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"},
+        {id:9, name: "Highway Assistance", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "gray", imgUrl: "https://images.unsplash.com/photo-1554672408-730436b60dde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"},
+        {id:10, name: "Insurance Coverage", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",color: "aquamarine", imgUrl: "https://images.unsplash.com/photo-1518430272387-254558840136?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=699&q=80"}
       ]
     };
 
@@ -63,7 +65,6 @@ class HomePage extends React.Component {
                     </div>
                   </section>
                   <section id="about">
-                    <h1>About</h1>
                     <div className="about-content-container">
                       {
                         this.props.services.map(x => <Box key={x.id} service={x}/>)
@@ -72,9 +73,16 @@ class HomePage extends React.Component {
                   </section>
                   <section id="services">
                     <h1>Services</h1>  
-                    <ul>
-                        <Link to="/vendor">Vendor services</Link>
-                    </ul>                  
+                    <div className="services-container">
+                        <div>
+                          <UserServiceSVG className="services-container-image"/>
+                        </div>
+                        <div>
+                          <Link to="/vendor">Vendor</Link>
+                          <VendorServiceSVG className="services-container-image"/>
+                        </div>
+                        {/*<Link to="/vendor">Vendor services</Link>*/}
+                    </div>                  
                   </section>
                   <section id="contact">Contact</section>          
               </main>
